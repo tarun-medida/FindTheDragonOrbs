ce3100016fd93c129e9922927fbeea1a4911e14d
tarun-medida <60532131+tarun-medida@users.noreply.github.com>
2024-02-20 16:42:04 +0530

    Changes made on 20 Feb.
    -Added the dialogue box to the Tutorial popups.

--- Assets/Scripts/TutorialPopUps.cs
+++ Assets/Scripts/TutorialPopUps.cs
@@ -1,57 +1,85 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class TutorialPopUps : MonoBehaviour
 {
-    public GameObject[] popUps;
-    private int popUpIndex;
+    public GameObject tutorialPanel;
+    public string[] popUps;
+    public TMP_Text tutorialText;
+    private int popUpIndex=0;
+    public float wordSpeed;
     //variable used for blocking player from entering play area
 
     public bool enterPlayArea = false;
+    private void Start()
+    {
+        tutorialPanel.SetActive(true);
+    }
     private void Update()
     {
-        for(int i = 0; i < popUps.Length-1; i++)
-        {
-            if (i == popUpIndex)
-            {
-                popUps[i].gameObject.SetActive(true);
-            }
-            else
-            {
-                popUps[i].gameObject.SetActive(false);
-            }
-        }
         if (popUpIndex == 0)
         {
-            if(Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.S))
+            TutorialPopUp(popUpIndex);
+            if (Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.S))
             {
                 popUpIndex++;
-                
             }
         }
         else if (popUpIndex == 1)
         {
-            if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+            TutorialPopUp(popUpIndex);
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
             {
                 popUpIndex++;
-                
             }
         }
-        else if (popUpIndex==2)
+        else if(popUpIndex == 2)
         {
+            TutorialPopUp(popUpIndex);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex==3)
+        {
+            TutorialPopUp(popUpIndex);
+            if (Input.GetKey(KeyCode.K))
+            {
+                popUpIndex++;
+            }          
+        }
+        else if (popUpIndex == 4)
+        {
+            TutorialPopUp(popUpIndex);
+            if (Input.GetKey(KeyCode.F))
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 5)
+        {
+            TutorialPopUp(popUpIndex);
             if (Input.GetKey(KeyCode.K))
             {
                 popUpIndex++;
                 enterPlayArea = true;
             }
-            
         }
-        else if (popUpIndex == 3)
+        else if (popUpIndex == popUps.Length)
         {
-            Destroy(popUps[popUpIndex]);
+            Destroy(tutorialPanel);
         }
+    }
 
+    private void TutorialPopUp(int popUpIndex)
+    {
+        tutorialText.SetText(popUps[popUpIndex]);
     }
 }
