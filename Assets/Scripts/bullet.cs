cfabcc663f174a6caa2abcd106a07f0bb79186b0
tarun-medida <60532131+tarun-medida@users.noreply.github.com>
2024-04-05 15:10:18 +0530

    Changes made on 5th April
    -Updated the minion projectile damage.
    -Updated the minion shooting distance.
    -Update the minion fire rate.
    -Update minion projectile to hit only player but not any other collider.

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+    public float attackDamage = 1.0f;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject, 1f);
+        
         /*
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
         Destroy(effect, 0.5f);
         */
+        if (collision.gameObject.tag == "Player")
+        //if(collision.tag == "Player")
+        {
+            IDamageable damageableObject = collision.collider.GetComponent<IDamageable>();
+            //IDamageable damageableObject = collision.GetComponent<IDamageable>();
+            if (damageableObject != null)
+            {
+                damageableObject.Hit(attackDamage);
+                Destroy(gameObject);
+            }
+        }
+        Destroy(gameObject, 5f);
     }
 
 }
