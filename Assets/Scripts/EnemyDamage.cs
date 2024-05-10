278b60e474e31b56524480e33b870b2b7ef13a06
tarun-medida <60532131+tarun-medida@users.noreply.github.com>
2024-05-10 16:53:00 +0530

    Changes made on 10th may
    -Added colliders to the enemy mele attack.

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    //Script used for dealing damage to the player.
+
+    public float attackDamage = 5.0f;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        //if(collision.tag == "Player")
+        {
+            IDamageable damageableObject = collision.collider.GetComponent<IDamageable>();
+            //IDamageable damageableObject = collision.GetComponent<IDamageable>();
+            if (damageableObject != null)
+            {
+                damageableObject.Hit(attackDamage);
+                Debug.Log("hit hit hit");
+            }
+        }
+    }
+}
